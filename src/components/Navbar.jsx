5b72383cbebb6ef9e67b59a0084fa46a9178ad99
camiloreyes1 <jreye236@fiu.edu>
2023-10-21 22:26:16 -0400

    styling commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = () => {
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="/" class="flex items-center">
   <img src="https://res.cloudinary.com/delscyuhi/image/upload/v1697939680/xna0rag5wqhfhbkzwryh.png" class="h-12 mr-3" alt="Flowbite Logo" />
-  <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Bro's Hot Chicken</span>
+  <span class="self-center text-2xl font-bold whitespace-nowrap text-white">Bro's Hot Chicken</span>
   </a>
   <div class="flex md:order-2">
       <a href="/" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Order Now</a>
 
       <a href="/menu" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Menu</a>
 
-      <a href="/locations" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Locations</a>
+      <a href="/about-us" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">About Us</a>
 
   </div>
   </div>
