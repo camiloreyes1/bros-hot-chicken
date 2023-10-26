c19ad21a8b164658c6208174fb6e1fe5d3f06cb8
camiloreyes1 <jreye236@fiu.edu>
2023-10-26 00:14:40 -0400

    carousel commit

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,98 +1,29 @@
-import React, { useState } from 'react';
 import { Navbar } from 'flowbite-react';
-
-
+import { Button } from 'flowbite-react';
 
 const NavbarComponent = () => {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
-
   return (
     <div>
-
-<Navbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
-        <img src="https://res.cloudinary.com/delscyuhi/image/upload/v1697939680/xna0rag5wqhfhbkzwryh.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Bro's Hot Chicken</span>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          About
-        </Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
-
-      {/* <nav className="bg-zinc-950 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="/" className="flex items-center">
-            <img src="https://res.cloudinary.com/delscyuhi/image/upload/v1697939680/xna0rag5wqhfhbkzwryh.png" className="h-12 mr-3" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-bold whitespace-nowrap text-white">Bro's Hot Chicken</span>
-          </a>
-          <button
-            onClick={toggleMenu}
-            className="md:hidden text-white focus:outline-none"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {isMenuOpen ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              )}
-            </svg>
-          </button>
-          <div
-            className={`md:flex md:items-center space-x-2 ${isMenuOpen ? 'block' : 'hidden'}`}
-          >
-            <a
-              href="/"
-              className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
-            >
-              Order Now
-            </a>
-            <a
-              href="/menu"
-              className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
-            >
-              Menu
-            </a>
-            <a
-              href="/about-us"
-              className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
-            >
-              About Us
-            </a>
-          </div>
-        </div>
-      </nav> */}
+      <Navbar className="bg-zinc-950 dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600" fluid>
+        <Navbar.Brand href="/">
+          <img src="https://res.cloudinary.com/delscyuhi/image/upload/v1697939680/xna0rag5wqhfhbkzwryh.png" className="mr-2 h-9 sm:h-9" alt="Logo" />
+          <span className="self-center whitespace-nowrap text-xl font-semibold text-white">Bro's Hot Chicken</span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Button href="#" className="btn-md text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm text-center mr-2 mb-2">
+            Order Now
+          </Button>
+          <Button href="#" className="btn-md text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover.bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm text-center mr-2 mb-2">
+            Menu
+          </Button>
+          <Button href="#" className="btn-md text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm text-center mr-2 mb-2">
+            About Us
+          </Button>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
 
 export default NavbarComponent;
-
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,37 @@
-import { Button } from 'flowbite-react';
-import React from 'react';
+import { Carousel } from "flowbite-react";
 
 const HomePage = () => {
     return (
-        <div className="container mx-4 px-5 py-20 flex flex-wrap items-center">
+        <div>
             <div> {/* Adjust the width and right padding as needed */}
                 <br></br>
                 <h1 className="text-center mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
                 >Welcome to Bro's Hot Chicken!</h1>
                 <br></br>
-                <h3>
+                <p className='text-center'>
                     Where we bring the fiery spirit of Nashville's famous hot chicken sandwich right to your plate!
-                </h3>
+                </p>
             </div>
-            <div className="w-70 p-10"> {/* Set the width to 30% */}
-                <img class="h-auto max-w-lg ml-auto"
+            
+            <div className="mb-4">
+            <Carousel>
+                <img
                     src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
-                    alt="Chicken-Sandwich"
-                    style={{ maxWidth: '50%' }} // Ensure the image resizes to fit the container
                 />
+                <img
+                    src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
+                />
+                <img
+                    src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
+                />
+                <img
+                    src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
+                />
+                <img
+                    src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
+                />
+            </Carousel>
+
             </div>
 
             <div>
@@ -26,87 +39,87 @@ const HomePage = () => {
                     Hours
                 </h1>
 
-        <div className="flex justify-center"> 
+                <div className="flex justify-center">
 
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400"  style={{ width: '100%' }}>
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                </thead>
-                <tbody>
-                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Monday
-                        </th>
-                        <td class="px-6 py-4">
-                            1:00 PM - 9:00 PM
-                        </td>
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400" style={{ width: '100%' }}>
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            </thead>
+                            <tbody>
+                                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Monday
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        1:00 PM - 9:00 PM
+                                    </td>
 
-                    </tr>
-                    <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Tuesday
-                        </th>
-                        <td class="px-6 py-4">
-                            1:00 PM - 9:00 PM
-                        </td>
+                                </tr>
+                                <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Tuesday
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        1:00 PM - 9:00 PM
+                                    </td>
 
-                    </tr>
-                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Wednesday
-                        </th>
-                        <td class="px-6 py-4">
-                            1:00 PM - 9:00 PM
-                        </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Wednesday
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        1:00 PM - 9:00 PM
+                                    </td>
 
-                    </tr>
-                    <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Thursday
-                        </th>
-                        <td class="px-6 py-4">
-                            1:00 PM - 11:00 PM
-                        </td>
+                                </tr>
+                                <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Thursday
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        1:00 PM - 11:00 PM
+                                    </td>
 
-                    </tr>
+                                </tr>
 
-                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Friday
-                        </th>
-                        <td class="px-6 py-4">
-                            1:00 PM - 11:00 PM
-                        </td>
+                                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Friday
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        1:00 PM - 11:00 PM
+                                    </td>
 
-                    </tr>
-                    <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Saturday
-                        </th>
-                        <td class="px-6 py-4">
-                            1:00 PM - 11:00 PM
-                        </td>
+                                </tr>
+                                <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Saturday
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        1:00 PM - 11:00 PM
+                                    </td>
 
-                    </tr>
+                                </tr>
 
-                    <tr>
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Sunday
-                        </th>
-                        <td class="px-6 py-4">
-                            1:00 PM - 7:00 PM
+                                <tr>
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Sunday
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        1:00 PM - 7:00 PM
 
-                        </td>
+                                    </td>
 
-                    </tr>
-                </tbody>
-            </table>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
         </div>
-
-        </div>
-    </div>
-</div>
-);
+    );
 };
 
 export default HomePage;
