94700455885a83f1ea0a8538e7cdd363df2c87c4
camiloreyes1 <jreye236@fiu.edu>
2023-11-01 23:21:10 -0400

    carousel commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,52 +14,14 @@ const HomePage = () => {
 
         <div>
             <div className="home-page" style={textStyle} >
-                <h1 className="text-center mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                <h1 className="text-center mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white">
                     Welcome to Bro's Hot Chicken!
                 </h1>
-                <p className="text-center" style={biggerParagraphStyle}>
-                    Where we bring the fiery spirit of Nashville's famous hot chicken sandwich right to your plate!
+                <p className="text-center text-white" style={biggerParagraphStyle}>
+                    Can You Handle the Heat?
                 </p>
             </div>
-            <div className="mt-4 mb-4">
 
-                <Carousel slide={false}>
-                    <Carousel.Item>
-                        <img className="d-block w-100"
-                            src="https://res.cloudinary.com/delscyuhi/image/upload/v1698893966/wopfnuhhypektpvs6zic.jpg" />
-                    </Carousel.Item>
-
-                    <Carousel.Item>
-                        <img className="d-block w-100"
-                            src="https://res.cloudinary.com/delscyuhi/image/upload/v1698893966/khw6glx7xfegkimth7bi.jpg" />
-                    </Carousel.Item>
-
-                   {/* <Carousel.Item>
-                        <img className="d-block w-100"
-                            src="https://res.cloudinary.com/delscyuhi/image/upload/v1698893966/b2yffqolxpzj7uennjac.jpg" />
-                    </Carousel.Item> */}
-                </Carousel>
-
-
-                {/* <Carousel>
-                <img
-                    src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
-                />
-                <img
-                    src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
-                />
-                <img
-                    src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
-                />
-                <img
-                    src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
-                />
-                <img
-                    src="https://res.cloudinary.com/delscyuhi/image/upload/v1697993599/BHC/usmsrzlwsigrh5lyy90y.png"
-                />
-            </Carousel> */}
-
-            </div>
             <div className="hours-section">
                 <br>
                 </br>
@@ -147,6 +109,27 @@ const HomePage = () => {
                     </div>
 
                 </div>
+            </div>
+            <br></br>
+            <div className="mb-4">
+
+                <Carousel className="h-50">
+                    <Carousel.Item>
+                        <img className="d-block w-100"
+                            src="https://res.cloudinary.com/delscyuhi/image/upload/v1698893966/wopfnuhhypektpvs6zic.jpg" />
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img className="d-block w-100"
+                            src="https://res.cloudinary.com/delscyuhi/image/upload/v1698893966/khw6glx7xfegkimth7bi.jpg" />
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img className="d-block w-100"
+                            src="https://res.cloudinary.com/delscyuhi/image/upload/v1698893966/b2yffqolxpzj7uennjac.jpg" />
+                    </Carousel.Item>
+                </Carousel>
+
             </div>
         </div>
     );
